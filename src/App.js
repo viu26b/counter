622cbf1dceabcdd64735faf5eb2691a8b0b44e0f
Victoria <victoria.udebiuwa@gmail.com>
2019-02-27 20:42:41 -0500

    Added counter functionality

--- src/App.js
+++ src/App.js
@@ -1,28 +1,37 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Pokemon, Gotta Catch em All!
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+class Counter extends Component {
+  constructor(){
+    super();
+    this.state = {
+      count: 0,
+    };
+  }
+
+  increment = () => {
+    this.setState ({
+      count: this.state.count + 1,
+    })
+  };
+
+  decrement = () => {
+    this.setState ({
+      count: this.state.count - 1,
+    })
+  };
+
+
+  render(){
+    return(<div className="container">
+            <div className="navbar">Counter.js</div>
+              <div className="counter">
+              <h1>{this.state.count}</h1>
+              <button type="button" onClick={this.increment}>Increment</button>
+              <button type="button" onClick={this.decrement}>Decrement</button>
+          </div>
       </div>
     );
   }
-}
+};
 
-export default App;
+export default Counter;
